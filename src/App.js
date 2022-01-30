0ed1d852247aedc57d3e177a4bf210d1d6bcd298
Giovanni Flores <gdr943@gmail.com>
2022-01-29 21:44:03 -0800

    init comit 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn NOTHING
         </a>
       </header>
     </div>
